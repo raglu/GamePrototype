d76607e0b473cb747e93c34fd3524a2212f39c5b
raglu <raglu16@student.sdu.dk>
2021-03-28 22:52:33 +0200

    Update Parser.java
    deleted comments

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -4,37 +4,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- * This class is part of the "World of Zuul" application.
- * "World of Zuul" is a very simple, text based adventure game.
- *
- * This parser reads user input and tries to interpret it as an "Adventure"
- * command. Every time it is called it reads a line from the terminal and
- * tries to interpret the line as a two word command. It returns the command
- * as an object of class Command.
- *
- * The parser has a set of known command words. It checks user input against
- * the known commands, and if the input is not one of the known commands, it
- * returns a command object that is marked as an unknown command.
- *
- * @author  Michael Kolling and David J. Barnes
- * @version 1.0 (February 2002)
- */
-
 public class Parser {
 
-    private CommandWords commands;  // holds all valid command words
+    private CommandWords commands;
 
     public Parser() {
         commands = new CommandWords();
     }
 
     public Command getCommand() {
-        String inputLine = "";   // will hold the full input line
+        String inputLine = "";
         String word1;
         String word2;
 
-        System.out.print("> ");     // print prompt
+        System.out.print("> ");
 
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(System.in));
